dfa0117bdbf807378cda979bec4974f55a56a252
root-hunter <a.ricciardi27@studenti.uniba.it>
2022-04-12 23:48:28 +0200

    Easy Version 1.1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-let start_x = 1000
-let start_y = 1000
-let start_size = 1
+let canvasSize = 2000
+
+let start_x = canvasSize/2
+let start_y = canvasSize/2
+let start_size = 0.1
 let group = []
+
 
 let state = 0 //from 0 to 3 for map square rotation
 
@@ -15,21 +18,21 @@ class Square{
     this.y = y
     this.size = size
   }
-  
+ 
   draw(){
-    let r = random(255); 
-    let g = random(100,200);
-    let b = random(100);
-    let a = random(200,255);
-  
+ 
     noStroke();
-    fill(r, g, b, a);
+    this.r = random(55); 
+    this.g = random(255);
+    this.b = random(55);
+    this.a = random(200,255);
+    fill(this.r, this.g, this.b, this.a);
     square(this.x, this.y, this.size)
   }
 }
 
 function fibonacci(n){
-  if(n === 1 || n === 0){
+  if(n < 2){
       return n
   }else{
     return fibonacci(n-1) + fibonacci(n-2)
@@ -37,23 +40,15 @@ function fibonacci(n){
 }
 
 function setup() {
-  createCanvas(2000, 2000);
+  createCanvas(canvasSize, canvasSize);
   group.push(new Square(start_x, start_y, start_size*m))
-}
 
-function draw() {
-  background(220);
-
-  for(let i = 0; i < group.length; ++i){
-    group[i].draw()
-  }
   
 
-  if(a < 40){
-    console.log(a+": "+fibonacci(a))
+  while(a < 20){
     old_size = start_size
     start_size = fibonacci(a+1)*m
-    console.log(state)
+    
     switch(state){
       case 0:
         start_y += old_size - fibonacci(a+1)*m
@@ -78,4 +73,14 @@ function draw() {
     
     a += 1
   }
+
+}
+
+function draw() {
+  background(220);
+
+  for(let i = 0; i < group.length; ++i){
+    group[i].draw()
+  }
+  
 }
