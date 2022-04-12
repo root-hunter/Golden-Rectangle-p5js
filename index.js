8f11a1a14b6d48ef311caf7ab70bca699b8482f5
root-hunter <a.ricciardi27@studenti.uniba.it>
2022-04-13 00:40:26 +0200

    Add spiral

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let canvasSize = 2000
+let canvasSize = 3000
 
 let start_x = canvasSize/2
-let start_y = canvasSize/2
-let start_size = 0.1
+let start_y = 1000
+let start_size = 1
 let group = []
 
 
@@ -13,22 +13,40 @@ let a = 1
 let m = 1
 
 class Square{
-  constructor(x, y, size){
+  constructor(x, y, size, state){
     this.x = x
     this.y = y
     this.size = size
+    this.r = random(55); 
+    this.g = random(80);
+    this.b = random(200, 255);
+    this.a = random(200,255);
+    this.state = state
   }
  
   draw(){
- 
-    noStroke();
-    this.r = random(55); 
-    this.g = random(255);
-    this.b = random(55);
-    this.a = random(200,255);
+   
     fill(this.r, this.g, this.b, this.a);
     square(this.x, this.y, this.size)
-  }
+    
+    noFill()
+    strokeWeight(4)
+    stroke("white")
+    switch(this.state){
+      case 0:
+        arc(this.x + this.size, this.y, this.size*2, this.size*2, PI/2, PI)
+        break;
+      case 1:
+        arc(this.x, this.y, this.size*2, this.size*2, 0, PI/2)
+        break;
+      case 2:
+        arc(this.x, this.y + this.size, this.size*2, this.size*2, -PI/2, 0)
+        break;
+      case 3:
+        arc(this.x + this.size, this.y + this.size, this.size*2, this.size*2, PI, -PI/2)
+        break;
+    }
+    }
 }
 
 function fibonacci(n){
@@ -41,11 +59,9 @@ function fibonacci(n){
 
 function setup() {
   createCanvas(canvasSize, canvasSize);
-  group.push(new Square(start_x, start_y, start_size*m))
+  group.push(new Square(start_x, start_y, start_size*m, state))
 
-  
-
-  while(a < 20){
+  while(a < 30){
     old_size = start_size
     start_size = fibonacci(a+1)*m
     
@@ -69,7 +85,7 @@ function setup() {
         state = 0;
         break;
     }
-    group.push(new Square(start_x, start_y, start_size))
+    group.push(new Square(start_x, start_y, start_size, state))
     
     a += 1
   }
@@ -82,5 +98,4 @@ function draw() {
   for(let i = 0; i < group.length; ++i){
     group[i].draw()
   }
-  
 }
